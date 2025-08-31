2874245b7a9bdfbcb9a20bc9d74acfd44c6a19af
Andrey Shorokhov <andreiv_shorokhov@mail.ru>
2025-08-31 08:20:45 +0500

    add interfaces

--- src/main/java/strategy/SortStrategy.java
+++ src/main/java/strategy/SortStrategy.java
@@ -1,4 +1,29 @@
+/**
+ created by Shorokhov Andrey
+ */
+
 package strategy;
 
-public interface SortStrategy {
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Интерфейс стратегия сортировки. Реализует паттерн Стратегия.
+ * Определяет контракт для различных алгоритмов сортировки.
+ * @param <T> тип сортируемых объектов
+ */
+public interface SortStrategy<T> {
+
+    /**
+     * Выполняет сортировку списка по заданному алгоритму.
+     * @param list список для сортировки
+     * @param comparator компаратор, определяющий порядок сортировки
+     */
+    void sort(List<T> list, Comparator<T> comparator);
+
+    /**
+     * Возвращает название алгоритма сортировки.
+     * @return строковое представление название алгоритма
+     */
+    String getAlgorithmName();
 }
