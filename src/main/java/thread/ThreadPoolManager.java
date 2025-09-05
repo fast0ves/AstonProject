1d9a3fb1b8f232e32bf1177ec0584f337036943d
Andrey Shorokhov <andreiv_shorokhov@mail.ru>
2025-09-05 16:02:06 +0500

    add threadPoolManager

--- src/main/java/thread/ThreadPoolManager.java
+++ src/main/java/thread/ThreadPoolManager.java
@@ -1,4 +1,31 @@
 package thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Менеджер пула потоков для сортировки.
+ * Создает минимум 2 потока, выполнение в ThreadPool
+ */
 public class ThreadPoolManager {
+    private final ExecutorService executor;
+
+    public ThreadPoolManager(ExecutorService executor) {
+        this.executor = Executors.newFixedThreadPool(2);
+    }
+
+    /**
+     * Выполняет задачу в пуле потоков
+     * @param task задача для выполнения
+     */
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+
+    /**
+     * Завершает работу пула потоков
+     */
+    public void shutdown() {
+        executor.shutdown();
+    }
 }
